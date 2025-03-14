2db0662d79899c34bee930886e825bfdffa7f83d
Aida Bah <aidasulayb@gmail.com>
2025-03-14 17:53:15 -0500

    Debug shell script

--- launch_sitl.sh
+++ launch_sitl.sh
@@ -7,9 +7,8 @@ PX4_DIR="$PARENT_DIR/PX4-Autopilot"
 QGC_APPIMAGE="$PARENT_DIR/QGroundControl/QGroundControl.AppImage"
 
 echo "Launching PX4 SITL..."
-wt -w 0 nt -p "Ubuntu-22.04" --title "PX$ SITL" bash -c "cd '$PX4_DIR'
-&& make px4_sitl_default gazebo-classic_plane; exec bash"
+xterm -e "cd '$PX4_DIR' && make px4_sitl_default gazebo-classic_plane; exec bash" &
 
 echo "Launching QGroundControl..."
 
-wt -w 0 nt -p "Ubuntu-22.04" --title "QGroundControl" bash -c "'$QGC_APPIMAGE'; exec bash"
+xterm -e "'$QGC_APPIMAGE'; exec bash" &
