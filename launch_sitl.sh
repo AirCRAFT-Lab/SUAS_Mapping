5524fa7a74e044a8eb3121f79b714c967858e6b2
aidabah <aidasulayb@gmail.com>
2025-03-13 23:20:03 -0500

    Fix typo in SITL setup shell script

--- launch_sitl.sh
+++ launch_sitl.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+SCRIPT_DIR="$(CD "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+PX4_DIR="$PARENT_DIR/PX4-Autopilot"
+QGC_APPIMAGE="$PARENT_DIR/QGroundControl/QGroundContro.AppImage"
+
 echo "Launching Gazebo SITL..."
 
-cd ~/PX4-Autopilot
+cd "$PX4_DIR"
 make px4_sitl_default gazebo-classic_plane
 
 echo "Launching QGroundControl..."
-cd ~/QGroundConrol
+cd "$QGC_APPIMAGE"
 ~./QGroundConrol.AppImage
