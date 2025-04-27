81406a16f62393c9c98e787e330af840bcfd3826
Aida Bah <aidasulayb@gmail.com>
2025-04-26 21:06:06 -0500

    Add scripts to clear images

---
+++ clear_TAGGED_images.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Set your image save folder path
+SAVE_DIR="/home/aidabah/test_images/TAGGED_test_images"
+
+# Remove image files
+rm -v "$SAVE_DIR"/*.png "$SAVE_DIR"/*.jpg 2>/dev/null
+
+echo "Image folder cleaned."
