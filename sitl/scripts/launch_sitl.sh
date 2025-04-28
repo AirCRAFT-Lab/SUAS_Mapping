b1c6141f4b949655e076d43f081277bc468dc90e
Aida Bah <aidasulayb@gmail.com>
2025-04-27 21:36:32 -0500

    Update launch script with new folder structure

--- sitl/scripts/launch_sitl.sh
+++ sitl/scripts/launch_sitl.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-PARENT_DIR="$(dirname "$SCRIPT_DIR")"
-PX4_DIR="$PARENT_DIR/PX4-Autopilot"
-QGC_APPIMAGE="$PARENT_DIR/QGroundControl/QGroundControl.AppImage"
+#SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+#PARENT_DIR="$(dirname "$SCRIPT_DIR")"
+PX4_DIR="/home/aidabah/PX4-Autopilot"
+QGC_APPIMAGE="/home/aidabah/QGroundControl/QGroundControl.AppImage"
 
 echo "Launching PX4 SITL..."
 xterm -e "cd '$PX4_DIR' && make px4_sitl_default gazebo-classic_suas_dragonfly__baylands; exec bash" &
