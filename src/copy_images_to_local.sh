78397283e7b63282a865380abb3c085a8a472da6
Aida Bah <aida.bah@slu.edu>
2025-05-30 22:20:40 -0500

    Improve documentation

--- src/copy_images_to_local.sh
+++ src/copy_images_to_local.sh
@@ -5,27 +5,29 @@ ssh suas@192.168.0.238 << 'EOF'
   echo "Starting Docker container..."
   docker start SUAS_GSTREAMER
 
-  echo "Finding latest Images directory..."
+
+  echo "Finding latest 'Images' directory..."
+  # Creates temporary directory to contain lates images
   latest_images_dir=$(docker exec SUAS_GSTREAMER find /ultralytics/Flight_tests -type d -name Images | sort | tail -n 1)
 
-  echo "Latest Images directory: $latest_images_dir"
+  echo "Latest 'Images' directory: $latest_images_dir"
 
-  # Ensure target directory is clean
+  # Check if target directory is clean
   if [ -d /home/suas/latest_images ]; then
-    echo "Removing previous latest_images directory..."
+    echo "Previous 'latest_images' directory found. Removing..."
     rm -rf /home/suas/latest_images
   fi
 
-  echo "Creating new latest_images directory..."
+  echo "Creating new 'latest_images' directory..."
   mkdir -p /home/suas/latest_images
 
-  echo "Copying latest Images directory to Jetson home..."
+  echo "Copying latest 'Images' directory to Jetson..."
   docker cp SUAS_GSTREAMER:$latest_images_dir /home/suas/latest_images
 
   echo "Done with Docker operations."
 EOF
 
-echo "Copying latest Images from Jetson to local machine..."
+echo "Copying latest 'Images' from Jetson to local machine..."
 scp -r suas@192.168.0.238:/home/suas/latest_images/Images/* ../data/raw
 
 echo "All done."
